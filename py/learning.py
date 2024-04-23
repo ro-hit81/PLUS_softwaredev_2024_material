d7ea5860bcc63dc2d9365d8c22feb0762d8c623d
Rohit Khati <rohitkhati@Rohits-MBP.wlan.sbg.ac.at>
2024-04-23 09:14:39 +0200

    Updated files for class on 22nd April.

---
+++ py/learning.py
@@ -0,0 +1,10 @@
+fruit = 'Mango'
+my_dict = {
+    'name': 'Rohit',
+    'surname': 'Khati'
+}
+
+def func_from_py(my_list):
+    print(f'There are {len(my_list)} players in my list.')
+    print('Now I will sort out the player name. ')
+    print(sorted(my_list))
